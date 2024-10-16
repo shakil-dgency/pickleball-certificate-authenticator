5bb511c78fff0bd87ec247f3821b4bbc345989cd
shakil <shakil.dgency@gmail.com>
2024-10-16 18:37:57 +0600

    test deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import localFont from "next/font/local";
+
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Roboto } from "next/font/google";
@@ -12,16 +12,6 @@ const roboto = Roboto({
 	weight: ["100", "300", "400", "500", "700", "900"], // Specify the available font weights here
 });
 
-// const geistSans = localFont({
-// 	src: "./fonts/GeistVF.woff",
-// 	variable: "--font-geist-sans",
-// 	weight: "100 900",
-// });
-// const geistMono = localFont({
-// 	src: "./fonts/GeistMonoVF.woff",
-// 	variable: "--font-geist-mono",
-// 	weight: "100 900",
-// });
 
 export const metadata = {
 	title: "Create Next App",
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,7 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      fontFamily: {
-        barlow: ['"Barlow"', 'sans-serif'], // Replace with your Google Font name
-      },
+    
     },
   },
   plugins: [],
